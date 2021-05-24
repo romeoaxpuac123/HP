83ddee405ac4fbc73d6f0544c03f9b8cac41bbfd
bayronaxpuac95 <48611884+romeoaxpuac123@users.noreply.github.com>
2021-05-24 01:05:48 -0600

    Galeria

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -7,9 +7,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  public Coleccion: string[] = [];
+  public Direcciones:string [] = [];
+  public i:number = 0;
+  constructor() { 
+    this.Coleccion = ["https://github.com/romeoaxpuac123/MisImagenes/blob/main/Diabetic/Banner6.jpg?raw=true",
+                     "https://github.com/romeoaxpuac123/MisImagenes/blob/main/Diabetic/Banner2.jpg?raw=true",
+                     "https://github.com/romeoaxpuac123/MisImagenes/blob/main/Diabetic/DMS.jpg?raw=true",
+                     "https://github.com/romeoaxpuac123/MisImagenes/blob/main/Diabetic/Banner1.jpg?raw=true"
+                  ]
+
+    this.Direcciones = ["https://www.facebook.com/HopeDiabetic",
+                      "",                      
+                      "https://www.diainternacionalde.com/ficha/dia-mundial-diabetes",
+                      "",
+    
+    ]
+    this.i +=1;
+
+
+  }
 
   ngOnInit(): void {
+    setInterval(() => {this.CambiarImagen()}, 6000);
+  }
+  CambiarImagen(){
+    let imagen: any = document.getElementById('slide');
+    let ruta: any = document.getElementById('ruta');
+    imagen.src = this.Coleccion[this.i];
+    ruta.href = this.Direcciones[this.i];
+    this.i = (this.i < this.Coleccion.length - 1) ? this.i+1 : 0;
   }
 
 }
--- src/app/components/pesentacion/pesentacion.component.ts
+++ src/app/components/pesentacion/pesentacion.component.ts
@@ -7,11 +7,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PesentacionComponent implements OnInit {
 
+  public Coleccion: string[] = [];
+  public Direcciones:string [] = [];
+  public i:number = 0;
   constructor() { 
+    this.Coleccion = ["https://github.com/romeoaxpuac123/MisImagenes/blob/main/Diabetic/Banner5.jpg?raw=true",
+    "https://github.com/romeoaxpuac123/MisImagenes/blob/main/Diabetic/Banner2.jpg?raw=true",
+    "https://github.com/romeoaxpuac123/MisImagenes/blob/main/Diabetic/Banner4.jpg?raw=true",
+    "https://github.com/romeoaxpuac123/MisImagenes/blob/main/Diabetic/Banner1.jpg?raw=true"
+ ]
+
+this.Direcciones = ["https://www.facebook.com/HopeDiabetic",
+     "",                      
+     "https://www.diainternacionalde.com/ficha/dia-mundial-diabetes",
+     "",
+
+]
    
   }
 
   ngOnInit(): void {
+    setInterval(() => {this.CambiarImagen()}, 5000);
+  }
+
+  CambiarImagen(){
+    let imagen: any = document.getElementById('slide');
+    let ruta: any = document.getElementById('ruta');
+    imagen.src = this.Coleccion[this.i];
+    ruta.href = this.Direcciones[this.i];
+    this.i = (this.i < this.Coleccion.length - 1) ? this.i+1 : 0;
   }
 
 }
